8bd61cde39946c7ad58da5a2f537df55289e3171
Tobusch <hm-tobusch@hm.edu>
2018-04-18 12:21:50 +0200

    task 2: system tick with diagram

--- embedded.c
+++ embedded.c
@@ -3,9 +3,10 @@
 #include <errno.h>
 #include <time.h>
 #include <sys/neutrino.h>
+#define BILLION 1000000000L
 
-void print_time(const struct timespec* time) {
-	printf("Current Time %d s, %ld ms\n", time->tv_sec, time->tv_nsec);
+void print_time(unsigned int time_waited_ns, unsigned int tick_size_ns) {
+	printf("%u;%u\n", time_waited_ns, tick_size_ns);
 }
 
 void changeSystemTick(unsigned int microsecs) {
@@ -27,30 +28,46 @@ void changeSystemTick(unsigned int microsecs) {
 }
 
 int main(int argc, char *argv[]) {
-	changeSystemTick(10); //min value for clockPeriod is 10us, otherwise Invalid Argument is thrown
+	const unsigned int start_tick_us = 1000;
+	const unsigned int end_tick_us = start_tick_us * 100;
+	changeSystemTick(1000); //min value for clockPeriod is 10us, otherwise Invalid Argument is thrown
 
 	int i;
 	struct timespec current;
+	struct timespec last;
 	if (-1 == clock_gettime(CLOCK_REALTIME, &current)) {
 		perror("clock gettime");
 		return EXIT_FAILURE;
 	}
-
-	print_time(&current);
-	for (i = 0; i < 100; i++) {
+	last = current;
+	for (i = start_tick_us; i < end_tick_us; i += 1000) {
+		changeSystemTick(i);
 		current.tv_nsec += 1000000; // 1ms
+		if (current.tv_nsec >= BILLION) {
+			current.tv_nsec -= BILLION;
+			current.tv_sec += 1;
+		}
 
-		if (-1 == clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &current, NULL)) {
+		if (-1
+				== clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &current,
+						NULL)) {
 			perror("clock nanosleep");
 			return EXIT_FAILURE;
 		}
 
-		if (-1 == clock_gettime(CLOCK_REALTIME, &current)) {
+		struct timespec after_sleep;
+		if (-1 == clock_gettime(CLOCK_REALTIME, &after_sleep)) {
 			perror("clock gettime");
 			return EXIT_FAILURE;
 		}
 
-		print_time(&current); // Assumption: takes less than 1ms
+		unsigned int time_slept_ns = 0;
+		time_slept_ns += (after_sleep.tv_sec - last.tv_sec) * BILLION;
+		time_slept_ns += (after_sleep.tv_nsec - last.tv_nsec);
+
+		print_time(time_slept_ns, i * 1000); // Assumption: takes less than 1ms
+		current = after_sleep;
+		last = current;
 	}
 
 	return EXIT_SUCCESS;
