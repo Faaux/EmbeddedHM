5238cf4aafa0ca343fd3aeb66241cbd1e13436f4
Tobusch Florian <tobusch@hm.edu>
2018-04-25 14:51:08 +0200

    Abgabe3

--- embedded.c
+++ embedded.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <math.h>
 #include <time.h>
 #include <sys/neutrino.h>
-#define BILLION 1000000000L
+#include <pthread.h>
+#include <sched.h>
 
-void print_time(unsigned int time_waited_ns, unsigned int tick_size_ns) {
-	printf("%u;%u\n", time_waited_ns, tick_size_ns);
-}
+#define BILLION 1000000000L
+#define ITERATIONS 90
 
 void changeSystemTick(unsigned int microsecs) {
 
@@ -22,53 +23,131 @@ void changeSystemTick(unsigned int microsecs) {
 		printf("Current clock: %ld us\n", old_period.nsec / 1000);
 		return;
 	}
-	printf("old clock: %ld us\n", old_period.nsec / 1000);
-	printf("new clock: %ld us\n", period.nsec / 1000);
 
 }
 
-int main(int argc, char *argv[]) {
-	const unsigned int start_tick_us = 1000;
-	const unsigned int end_tick_us = start_tick_us * 100;
-	changeSystemTick(1000); //min value for clockPeriod is 10us, otherwise Invalid Argument is thrown
+float waste_scaling = 1.f;
+
+void waste_msec_internal() {
+	volatile int first = 0, second = 1, next, c;
+	for (c = 0; c < (int) (200000 * waste_scaling); c++) {
+		if (c <= 1) {
+			next = c;
+		} else {
+			next = first + second;
+			first = second;
+			second = next;
+		}
+	}
+}
+
+void calibrate_waste_scaling() {
+	struct timespec current;
+	struct timespec after;
+	waste_scaling = 1.f;
+
+	if (-1 == clock_gettime(CLOCK_REALTIME, &current)) {
+		perror("clock_gettime");
+	}
 
 	int i;
+	for (i = 0; i < ITERATIONS; i++) {
+		waste_msec_internal();
+	}
+
+	if (-1 == clock_gettime(CLOCK_REALTIME, &after)) {
+		perror("clock_gettime");
+	}
+
+	float time_slept_ms = 0;
+	time_slept_ms += (after.tv_sec - current.tv_sec) * 1000.f;
+	time_slept_ms += (after.tv_nsec - current.tv_nsec) / (1000.f * 1000.f);
+	time_slept_ms /= ITERATIONS;
+	if (fabsf(time_slept_ms) < 0.01f) {
+		printf("calibration failed\n");
+		return;
+	}
+	waste_scaling = 1.f / time_slept_ms;
+	printf("Time Slept ms: %f\n", time_slept_ms);
+	printf("Waste_scaling: %f\n", waste_scaling);
+}
+
+void waste_msec(unsigned int msecs) {
+
+	int i;
+	for (i = 0; i < msecs; i++)
+		waste_msec_internal();
+}
+
+void do_waste_stess_test() {
+	unsigned int time_to_sleep_ms = 10;
+
 	struct timespec current;
-	struct timespec last;
+	struct timespec after;
 	if (-1 == clock_gettime(CLOCK_REALTIME, &current)) {
-		perror("clock gettime");
-		return EXIT_FAILURE;
+		perror("clock_gettime");
 	}
-	last = current;
-	for (i = start_tick_us; i < end_tick_us; i += 1000) {
-		changeSystemTick(i);
-		current.tv_nsec += 1000000; // 1ms
-		if (current.tv_nsec >= BILLION) {
-			current.tv_nsec -= BILLION;
-			current.tv_sec += 1;
-		}
 
-		if (-1
-				== clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &current,
-						NULL)) {
-			perror("clock nanosleep");
-			return EXIT_FAILURE;
-		}
-
-		struct timespec after_sleep;
-		if (-1 == clock_gettime(CLOCK_REALTIME, &after_sleep)) {
-			perror("clock gettime");
-			return EXIT_FAILURE;
-		}
-
-		unsigned int time_slept_ns = 0;
-		time_slept_ns += (after_sleep.tv_sec - last.tv_sec) * BILLION;
-		time_slept_ns += (after_sleep.tv_nsec - last.tv_nsec);
-
-		print_time(time_slept_ns, i * 1000); // Assumption: takes less than 1ms
-		current = after_sleep;
-		last = current;
+	int i;
+	for (i = 0; i < ITERATIONS; i++) {
+		waste_msec(time_to_sleep_ms);
 	}
+
+	if (-1 == clock_gettime(CLOCK_REALTIME, &after)) {
+		perror("clock_gettime");
+	}
+
+	float time_slept_ms = 0;
+	time_slept_ms += (after.tv_sec - current.tv_sec) * 1000.f;
+	time_slept_ms += (after.tv_nsec - current.tv_nsec) / (1000.f * 1000.f);
+
+	printf("Time slept: %f (ms)\n", time_slept_ms);
+	printf("Time error absolute: %f (ms)\n",
+			fabsf(ITERATIONS * time_to_sleep_ms - time_slept_ms));
+}
+
+int get_thread_priority() {
+	struct sched_param sched_param;
+
+	if (EOK != pthread_getschedparam(pthread_self(), NULL, &sched_param)) {
+		perror("pthread_getschedparam()");
+	}
+
+	return sched_param.sched_priority;
+}
+
+void* worker_thread(void* ignored) {
+
+	printf("Thread priority: %i\n", get_thread_priority());
+
+	calibrate_waste_scaling();
+	do_waste_stess_test();
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	changeSystemTick(1000);
+
+	printf("process priority: %i\n", get_thread_priority());
+	calibrate_waste_scaling();
+	do_waste_stess_test();
+
+	pthread_t worker;
+	pthread_attr_t worker_attr;
+
+	struct sched_param worker_sched_param;
+	worker_sched_param.sched_priority = sched_get_priority_max(SCHED_RR);
+
+	pthread_attr_init(&worker_attr);
+	pthread_attr_setinheritsched(&worker_attr, PTHREAD_EXPLICIT_SCHED);
+	pthread_attr_setschedparam(&worker_attr, &worker_sched_param);
+
+	if (EOK != pthread_create(&worker, &worker_attr, &worker_thread, NULL)) {
+		perror("Error during thread creation");
+	}
+
+	pthread_join(worker, NULL);
 
 	return EXIT_SUCCESS;
 }
